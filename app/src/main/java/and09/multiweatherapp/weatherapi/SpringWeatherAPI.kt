7cbd39750f0fd74faa5ae5a4b3300abe3a9b7482
Nita W <nitawasniowski@gmail.com>
2023-11-29 04:26:13 +0100

    SpringWeatherAPI added test upgraded

---
+++ app/src/main/java/and09/multiweatherapp/weatherapi/SpringWeatherAPI.kt
@@ -0,0 +1,55 @@
+package and09.multiweatherapp.weatherapi
+
+import and09.multiweatherapp.HttpRequest
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.net.URLEncoder
+
+class SpringWeatherAPI private constructor(queryString: String) : WeatherAPI {
+    private val weatherdata: JSONObject
+
+    companion object {
+        private const val BASE_URL = "http://192.168.1.105:8080/weather?"
+
+        @Throws(IOException::class, JSONException::class)
+        fun fromLocationName(locationName: String?):
+                WeatherAPI = SpringWeatherAPI(
+            "location=" +
+                    URLEncoder.encode(locationName, "UTF-8")
+        )
+
+
+        @Throws(IOException::class, JSONException::class)
+        fun fromLatLon(lat: Double, lon: Double):
+                WeatherAPI = SpringWeatherAPI("query=$lat,$lon")
+
+    }
+
+    init {
+        val result = HttpRequest.request(BASE_URL + queryString)
+        weatherdata = JSONObject(result)
+        println(weatherdata.toString()) // Debug!
+    }
+
+    @get:Throws(JSONException::class)
+    override val temperature: Int
+        get() = weatherdata.getInt("temperature")
+
+    @get:Throws(JSONException::class)
+    override val description: String
+        get() = weatherdata.getString("description")
+
+    @get:Throws(JSONException::class)
+    override val iconUrl: String
+        get() = weatherdata.getString("iconUrl")
+
+    @get:Throws(JSONException::class)
+    override val location: String
+        get() = weatherdata.getString("locationName")
+
+    @get:Throws(JSONException::class)
+    override val providerUrl: String
+        get() = weatherdata.getString("providerUrl")
+
+}
--- app/src/test/java/and09/multiweatherapp/ExampleUnitTest.kt
+++ app/src/test/java/and09/multiweatherapp/ExampleUnitTest.kt
@@ -1,6 +1,7 @@
 package and09.multiweatherapp
 
 import and09.multiweatherapp.weatherapi.OpenWeatherMapAPI
+import and09.multiweatherapp.weatherapi.SpringWeatherAPI
 import and09.multiweatherapp.weatherapi.WeatherStackAPI
 import org.json.JSONException
 import org.junit.Test
@@ -49,5 +50,25 @@ class ExampleUnitTest {
         println("Icon: ${api.iconUrl}")
         println("Location: ${api.location}")
         print("Provider:${api.providerUrl}")
+    }
+
+    @Test
+    @Throws(IOException::class, JSONException::class)
+    fun springWeatherAPI_getResponseFromName() {
+        val api = SpringWeatherAPI.fromLocationName("San Francisco")
+        println("Temp: ${api.temperature}")
+        println("Description: ${api.description}")
+        println("Icon: ${api.iconUrl}")
+        println("Location: ${api.location}")
+    }
+
+    @Test
+    @Throws(IOException::class, JSONException::class)
+    fun springWeatherAPI_getResponseFromLatLon() {
+        val api = SpringWeatherAPI.fromLatLon(37.77, -122.42)
+        println("Temp: ${api.temperature}")
+        println("Description: ${api.description}")
+        println("Icon: ${api.iconUrl}")
+        println("Location: ${api.location}")
     }
 }
